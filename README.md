216df7a457789f3bfc90770c123c92b7d8cb9556
DH <kdh6429@yahoo.co.kr>
2020-05-25 00:50:54 +0900

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# youtube-meta-parser
+A module to extract meta information from YouTube page.
+This is not required any google api and extract the data from youtube view page.
+
+##Install
+npm install youtube-meta-parser
+
+## Usage
+```
+var parser = require('youtube-meta-parser');
+parser.getMetadata("ZI4tRn4dOGg").then( function(metadata) {
+        console.log(metadata['videoDetails']['title']);
+        console.log(metadata['videoDetails']['lengthSeconds']);
+        console.log(metadata['videoDetails']['viewCount']);
+        console.log(metadata['videoDetails']['averageRating']);
+        console.log(metadata['videoDetails']['author']);
+    }
+)
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+var Promise = require('bluebird');
+const fetch = require('node-fetch')
+
+function extractConfigData(html) {
+  var index,
+      start = 'ytplayer.config = ',
+      //end = '</script>';
+      end = ';ytplayer.load';
+
+  index = html.indexOf(start);
+  if (index === -1) {
+    return '';
+  }
+
+  html = html.slice(index + start.length);
+
+  index = html.indexOf(end);
+  if (index === -1) {
+    return '';
+  }
+
+  return html.slice(0, index);
+}
+function fetchMetadata(id) {
+    var url = 'http://www.youtube.com/watch?v=' + id;
+
+    return new Promise(function (fulfill, reject) {
+      var url = 'http://www.youtube.com/watch?v=' + id;
+      return fetch(url)
+      .then(res => res.text())
+      .then(body => {
+        jsonStr = extractConfigData(body);
+        jsonObj = JSON.parse(jsonStr)
+        jsonObj = JSON.parse( jsonObj['args']['player_response']);
+        //console.log( JSON.parse( jsonObj['args']['player_response']));
+
+        fulfill(jsonObj);
+      })
+    });
+}
+
+module.exports = {
+    getMetadata: function (videoID) {
+      return new Promise(function (fulfill, reject) {
+        if (!videoID) {
+          reject(new Error('Unable to get video id.'));
+          return;
+        }
+        fetchMetadata(videoID).then(
+          function (d) {
+            fulfill( d);
+          }
+        );
+      });
+    }
+};
